d6be3aaeed3e656680fdecb475bc94aacfa10de1
Marcus Chiu <marcuschiu9@gmail.com>
2020-12-31 16:52:28 -0600

    added Zuul Filter example

--- README.md
+++ README.md
@@ -48,3 +48,8 @@ zuul:
       path: /default/test
       url: http://localhost:8080
 ```
+
+## Relevant Articles
+- [Rate Limiting in Spring Cloud Netflix Zuul](https://www.baeldung.com/spring-cloud-zuul-rate-limit)
+- [Spring REST with a Zuul Proxy](https://www.baeldung.com/spring-rest-with-zuul-proxy)
+- [Modifying the Response Body in a Zuul Filter](https://www.baeldung.com/zuul-filter-modifying-response-body)
--- src/main/java/com/marcuschiu/zuulexample/api/controller/DefaultController.java
+++ src/main/java/com/marcuschiu/zuulexample/api/controller/DefaultController.java
@@ -4,12 +4,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 @RequestMapping("/default")
 public class DefaultController {
 
     @GetMapping("/test")
     public String get() {
+        return "Hello, World!";
+    }
+
+    @GetMapping("/header")
+    public String header(HttpServletRequest req, HttpServletResponse res) {
+        if (req.getHeader("Test") != null) {
+            res.addHeader("Test", req.getHeader("Test"));
+        }
         return "Hello, World!";
     }
 }
